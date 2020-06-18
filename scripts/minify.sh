a74fa137223bab882bea354d5b073c2b9834cc8e
Fabien Crespel <fabien@crespel.net>
2020-06-18 22:37:39 +0200

    Add minify script

---
+++ scripts/minify.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SCRIPT_DIR=`dirname "$0"`
+CSS_DIR="$SCRIPT_DIR/../css"
+JS_DIR="$SCRIPT_DIR/../js"
+YUIC_JAR="$SCRIPT_DIR/yuicompressor-2.4.7.jar"
+YUIC="java -jar $YUIC_JAR"
+
+# Minify JavaScript
+$YUIC -o "$JS_DIR/global-min.js" "$JS_DIR/global.js"
+
+# Minify CSS
+cat "$CSS_DIR/layout.css" "$CSS_DIR/common.css" "$CSS_DIR/screen.css" | $YUIC -o "$CSS_DIR/style-min.css" --type css
+$YUIC -o "$CSS_DIR/layout-min.css" "$CSS_DIR/layout.css"
+$YUIC -o "$CSS_DIR/tinymce-min.css" "$CSS_DIR/tinymce.css"
