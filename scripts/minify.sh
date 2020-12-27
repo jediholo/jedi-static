b06f14c8c876c475b25d6fddc82112e22e065c22
Fabien Crespel <fabien@crespel.net>
2020-12-27 14:38:33 +0100

    Update CSS for buttons

--- scripts/minify.sh
+++ scripts/minify.sh
@@ -5,12 +5,19 @@ CSS_DIR="$SCRIPT_DIR/../css"
 JS_DIR="$SCRIPT_DIR/../js"
 YUIC_JAR="$SCRIPT_DIR/yuicompressor-2.4.7.jar"
 YUIC="java -jar $YUIC_JAR"
+JS_LIST="global"
+CSS_LIST="colorbox layout tinymce"
 
 # Minify JavaScript
-$YUIC -o "$JS_DIR/global-min.js" "$JS_DIR/global.js"
+for FILE in $JS_LIST; do
+	echo "Minifying $FILE.js ..."
+	$YUIC -o "$JS_DIR/$FILE-min.js" "$JS_DIR/$FILE.js"
+done
 
 # Minify CSS
+for FILE in $CSS_LIST; do
+	echo "Minifying $FILE.css ..."
+	$YUIC -o "$CSS_DIR/$FILE-min.css" "$CSS_DIR/$FILE.css"
+done
+echo "Minifying style.css ..."
 cat "$CSS_DIR/layout.css" "$CSS_DIR/common.css" "$CSS_DIR/screen.css" | $YUIC -o "$CSS_DIR/style-min.css" --type css
-$YUIC -o "$CSS_DIR/colorbox-min.css" "$CSS_DIR/colorbox.css"
-$YUIC -o "$CSS_DIR/layout-min.css" "$CSS_DIR/layout.css"
-$YUIC -o "$CSS_DIR/tinymce-min.css" "$CSS_DIR/tinymce.css"
